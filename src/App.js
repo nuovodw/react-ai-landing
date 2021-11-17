fe3b06fb9de7c674364a8f1a080beabff5e727dd
Daniel Won <dan.j.won@gmail.com>
2021-11-17 06:20:26 -0600

    import components and containers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
+import {
+	Footer,
+	Blog,
+	Possibility,
+	Features,
+	WhatGPT3,
+	Header,
+} from './containers';
+
+import { Cta, Brand, Navbar } from './components';
+
 const App = () => {
 	return (
 		<div>
-			<h1>Home page</h1>
+			<div className="App">
+				<div className="gradient__bg">
+					<Navbar />
+					<Header />
+				</div>
+				<Brand />
+				<WhatGPT3 />
+				<Features />
+				<Possibility />
+				<Cta />
+				<Blog />
+				<Footer />
+			</div>
 		</div>
 	);
 };
